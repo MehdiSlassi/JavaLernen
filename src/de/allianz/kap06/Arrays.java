f9bce9e360bc322e3d60c51970ffda23933b99c1
Mehdi Slassi <mehdi.slassi@allianz.de>
2019-08-07 16:40:20 +0200

    bagfix KlasseArrays

--- src/de/allianz/kap06/Arrays.java
+++ src/de/allianz/kap06/Arrays.java
@@ -3,12 +3,17 @@ package de.allianz.kap06;
 public class Arrays {
 
 	public static void main(String[] args) {
+		
+		int x = 1;
 		int[] bArray = new int[4];
-		for (int i = 0; i < bArray.length -1;  i++) {
+		for (int i = 0; i < bArray.length;  i++) {
 			bArray[i] = i*i;
 			System.out.println("Der Wert der Array-komponente " + i + " ist " + bArray[i]);
 		}
 		
+		int[] bArray1 = {x, x+x, 2*x, 3*x};
+		
+		System.out.println("Der Wert der Array-komponente 2  ist " + bArray1[1]);
 	}
 
 }
